b70ec49ef86edb176fd58c0bc6ab528c36dff31a
Phillip Lagoc <philliplagoc@gmail.com>
2024-07-03 18:03:41 -0700

    removed hello() test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -11,17 +11,3 @@ def test_config():
     """Test create_app() without passing test config."""
     assert not create_app().testing
     assert create_app({"TESTING": True}).testing
-
-
-def test_hello(client):
-    """Tests the hello() function.
-
-    Checks if the data of the page has the phrase
-    "Hello, World!"
-
-    Args:
-        client (flask.testing.FlaskClient): A test client that
-            makes requests.
-    """
-    response = client.get("/hello")
-    assert response.data == b"Hello, World!"
