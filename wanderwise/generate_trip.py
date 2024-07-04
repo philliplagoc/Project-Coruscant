a606500092f5c095b24a1991bba1dc2f0866120f
Phillip Lagoc <philliplagoc@gmail.com>
2024-07-03 23:12:08 -0700

    showing the destination inputted on the index page now

--- wanderwise/generate_trip.py
+++ wanderwise/generate_trip.py
@@ -13,12 +13,13 @@ bp = Blueprint("generate_trip", __name__)
 
 @bp.route("/", methods=("GET", "POST"))
 def index():
-    """Show a text area where the user inputs a 
+    """Show a text area where the user inputs a
        destination and AI generates a trip itinerary.
 
     Returns:
         str: A rendered template of the index.html page.
     """
+    destination = ""
     # User entered a destination
     if request.method == "POST":
         destination = request.form["destination"].strip()
@@ -26,8 +27,5 @@ def index():
         # Ensure that the user didn't send in an empty string.
         if not destination:
             flash("Destination is required.")
-            return render_template("generate_trip/index.html")
 
-        return f"You want to visit: {destination}"
-    
-    return render_template("generate_trip/index.html")
+    return render_template("generate_trip/index.html", destination=destination)
